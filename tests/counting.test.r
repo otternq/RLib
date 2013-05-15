5372e87d080e9d1a7a8c8ab1fc86e47875a53a40
otternq <otternq@gmail.com>
2013-05-14 18:09:31 -0700

    #3 adding permutations and combinations with unit tests

---
+++ tests/counting.test.r
@@ -0,0 +1,7 @@
+test.combinations <- function () {
+    checkEquals( combination(10, 3), 120, "Basic Combination")
+}
+
+test.permutations <- function () {
+    checkEquals( permutations(10, 3), 720, "Basic Combination")
+}
