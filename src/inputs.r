19adb9bb8c6565b10dc8951d43056569175e02d6
otternq <otternq@gmail.com>
2013-05-14 18:40:09 -0700

    fixing fileToMatrix function

--- src/inputs.r
+++ src/inputs.r
@@ -1,3 +1,3 @@
-fileToMatrix <- function(fileName) {
-    return(as.matrix(read.delim(fileName, header=FALSE, sein
+fileToMatrix <- function(fileName, tempHeader=FALSE, tempSep = "") {
+    return(as.matrix(read.delim(fileName, header=tempHeader, colClasses = "numeric", sep = tempSep)))
 }
